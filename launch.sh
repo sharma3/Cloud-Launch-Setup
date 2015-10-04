3482eb87a711d9f7377da63d0b0af4476162e7bd
Jay Sharma <jsharma3@hawk.iit.edu>
2015-10-04 05:42:35 -0500

    updated launch.sh file

--- launch.sh
+++ launch.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-﻿aws ec2 run-instances --image-id ami-$1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --user-data file://ITMO-544-A20344475-Enviornment-Setup/install-env.sh --debug
+aws ec2 run-instances --image-id ami-$1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --user-data file://~/Documents/ITMO-544-A20344475-Enviornment-Setup/install-env.sh --debug
 
