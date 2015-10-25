439d36b9fd72b5b7e5989c199ce93b9bab344e23
Jay Sharma <jsharma3@hawk.iit.edu>
2015-10-25 18:20:55 -0500

    Added sleep command

--- launch.sh
+++ launch.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 aws ec2 run-instances --image-id ami-$1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --iam-profile $7 --user-data file://~/Documents/ITMO-544-A20344475-Enviornment-Setup/install-env.sh --debug
 
+
+sleep 180
+
