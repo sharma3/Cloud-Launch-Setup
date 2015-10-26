d52546078fa78562aebd9b62cc05dbd0b17cffcf
Jay Sharma <jsharma3@hawk.iit.edu>
2015-10-26 18:06:17 -0500

    added load balancer and cleanup.sh file

--- launch.sh
+++ launch.sh
@@ -7,7 +7,7 @@ declare -a instanceIDARR
 
 #Launch instance
 
-mapfile -t instanceIDARR = (aws ec2 run-instances --image-id ami-$1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --iam-instance-profile Name=$7 --user-data file://~/Documents/ITMO-544-A20344475-Enviornment-Setup/install-env.sh --output table | grep InstanceId | sed "s/|//g" | tr -d ' '| sed "s/InstanceId//g")
+mapfile -t instanceIDARR < <(aws ec2 run-instances --image-id ami-$1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --iam-instance-profile Name=$7 --user-data file://~/Documents/ITMO-544-A20344475-Enviornment-Setup/install-env.sh --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
 
 #Calling Instance Array
 echo ${instanceIDARR[@]}
@@ -16,4 +16,15 @@ echo ${instanceIDARR[@]}
 aws ec2 wait instance-running --instance-ids ${instanceIDARR[@]}
 echo "Instances are Running"
 
+#Load Balancer
+ELBURL=(`aws elb create-load-balancer --load-balancer-name $8 --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --subnets $6 --security-groups $5 --output=text`); echo $ELBURL
+echo -e "\nFinished launching ELB and sleeping 25 seconds"
+for i in {0..25}; do echo -ne '.'; sleep 1;done
+
+aws elb register-instances-with-load-balancer --load-balancer-name $8 --instances ${instanceIDARR[@]}
+
+aws elb configure-health-check --load-balancer-name $8 --health-check Target=HTTP:80 index.html,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
+
+echo -e "\nWaiting an additional 180 sec - before opening the load balancer in a web browser"
+for i in {0..180}; do echo -ne '.'; sleep 1;done
 
